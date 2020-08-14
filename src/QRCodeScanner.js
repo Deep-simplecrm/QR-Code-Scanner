393309d01de8cbcc9540c1fd7cdc659536539fba
deep-khode <deep@simplecrm.com.sg>
2020-08-14 11:54:47 +0530

    Image changes

--- src/QRCodeScanner.js
+++ src/QRCodeScanner.js
@@ -27,7 +27,7 @@ class QRCodeScanner extends Component {
   }
   getJsonFormatData(data){
     const res = data.split(',');
-    console.log(res);
+    console.log(data);
     let email_id = res[3].split(': ');
     let phone = res[2].split(': ');
     let adderss_res = res[4].split(': ');
@@ -35,10 +35,32 @@ class QRCodeScanner extends Component {
     let address = adderss_res[1]+ ',' + res[5]+ ',' + res[6]+ ',' + res[7];
     let extractJsonData = {Name:res[0], Email:email_id[1], Phone:phone[1], Address:address, Website:res[8], image: image_res[1]};
     this.setState({jsonData: extractJsonData, displayScanner: false});
+    const ctx = this.refs.canvas.getContext('2d');
+        // ctx.clearRect(300,300, 300, 300);
+        var r, g, b;
+        const image_rgb = this.state.jsonData.image;
+        // console.log(image_rgb);
+        for (var i = 0; i < image_rgb.length; i++) {
+            for (var j = 0; j < image_rgb[0].length; j++) {
+                r = image_rgb[i][j][0];
+                g = image_rgb[i][j][1];
+                b = image_rgb[i][j][2];
+                ctx.fillStyle = "rgba(" + r + "," + g + "," + b + ", 1)";
+                ctx.fillRect(j, i, 1, 1);
+                // console.log(i)
+            }
+        }
+        console.log("complete");
+        // this.rect({ctx, x: 10, y: 10, width: 50, height: 50});
+        // this.rect({ctx, x: 110, y: 110, width: 50, height: 50});
     // const canvas = createCanvas(200, 200);
     // const ctx = canvas.getContext('2d', { pixelFormat: 'A8' });
     // console.log(ctx.putImageData(image_res[1]);
   }
+rect(props) {
+    const {ctx, x, y, width, height} = props;
+    ctx.fillRect(x, y, width, height);
+}
   handleError(err){
     console.error(err);
   }
@@ -88,7 +110,7 @@ class QRCodeScanner extends Component {
                     </tr>
                     <tr>
                       <td>Image</td>
-                      <td></td>
+                      <td><canvas ref="canvas" width={300} height={300}/></td>
                     </tr>
                   </tbody>
                 </table>
