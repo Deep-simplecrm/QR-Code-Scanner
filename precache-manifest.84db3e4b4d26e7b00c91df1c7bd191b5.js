d9c02cedef2c86f039cb477782f3a65c5cfc2c35
deep-khode <deep@simplecrm.com.sg>
2020-08-27 12:07:04 +0530

    Updates

---
+++ precache-manifest.84db3e4b4d26e7b00c91df1c7bd191b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7c12dce9d950ee3668ab1dfd5d8ac7b",
+    "url": "/QR-Code-Scanner/index.html"
+  },
+  {
+    "revision": "487f371b69bf15e0e9e8",
+    "url": "/QR-Code-Scanner/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0c2240416b5f8f425011",
+    "url": "/QR-Code-Scanner/static/js/2.d9b20664.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QR-Code-Scanner/static/js/2.d9b20664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "487f371b69bf15e0e9e8",
+    "url": "/QR-Code-Scanner/static/js/main.7fca627a.chunk.js"
+  },
+  {
+    "revision": "3be1d57ae2f3b51cd513",
+    "url": "/QR-Code-Scanner/static/js/runtime-main.31d8737c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/QR-Code-Scanner/static/media/logo.5d5d9eef.svg"
+  }
+]);
